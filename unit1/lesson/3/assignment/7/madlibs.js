3f753d01bacf86667f6f23ce2b84d4bcdaf56fa8
Juan <jualin289@gmail.com>
2014-07-01 20:44:58 -0400

    End of Lesson 3, Assignment 7

--- unit1/lesson/3/assignment/7/madlibs.js
+++ unit1/lesson/3/assignment/7/madlibs.js
@@ -12,6 +12,16 @@ angular.module('myApp',[])
       $scope.user = angular.copy($scope.master);
       $scope.results = angular.copy($scope.master);
       $scope.startOver = 'true';
+      $scope.tipTotal = 0;
+      $scope.mealCount=0;
+      $scope.averageTip= 0;
+      $scope.subtotal = 0;
+      $scope.tip=0;
+      $scope.total=0;
+    };
+
+    $scope.cancel = function() {
+      $scope.user = angular.copy($scope.master);
     };
 
     $scope.login = {

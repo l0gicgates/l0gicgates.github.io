18ecff3746bb13943b65913c77f5ec7657774473
Juan <jualin289@gmail.com>
2014-07-10 00:46:33 -0400

    Instagram searcher

--- unit1/lesson/5/assignment/5/madlibs.js
+++ unit1/lesson/5/assignment/5/madlibs.js
@@ -1,49 +1,46 @@
-angular.module('myApp',[])
-  .constant('VERSION', 1.1)
-  .controller('MyCtrl', function(VERSION, $scope) {
-    $scope.version = VERSION;
+angular.module("myApp", [])
+    .factory('instagram', ['$http',
+        function($http) {
+            return {
+                fetch: function(tag,callback) {
 
 
- 	$scope.master = {};
-  $scope.tipTotal = 0;
-  $scope.mealCount=0;
-  $scope.averageTip= 0;
- 	$scope.reset = function() {
-      $scope.user = angular.copy($scope.master);
-      $scope.results = angular.copy($scope.master);
-      $scope.startOver = 'true';
-      $scope.tipTotal = 0;
-      $scope.mealCount=0;
-      $scope.averageTip= 0;
-      $scope.subtotal = 0;
-      $scope.tip=0;
-      $scope.total=0;
-    };
+                    var endPoint = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?access_token=1421904868.1fb234f.1727c988209f41d38719695b3e315318&max_tag_id=1404965770620702&callback=JSON_CALLBACK";
 
-    $scope.cancel = function() {
-      $scope.user = angular.copy($scope.master);
-    };
-
-    $scope.login = {
+                    $http.jsonp(endPoint).success(function(response) {
+                        callback(response.data);
+                    });
+                }
+            }
+        }
+    ])
+    .controller("Example", function($scope, $interval, instagram) {
+      $scope.pics = [];
+      $scope.have = [];
+      $scope.getMore = function(tag) {
+        instagram.fetch(tag, function(data) {
+            for(var i=0; i<data.length; i++) {
+              if (typeof $scope.have[data[i].id]==="undefined") {
+                $scope.pics.push(data[i]) ;
+                $scope.have[data[i].id] = "1";
+              }
+            }
+        });
+      };
+      $scope.login = {
         submit: function() {
           var user = $scope.user; // for easier access
             if($scope.loginform.$invalid) {
-            	 alert("Please enter required fields");
-            	return false;
+               alert("Please enter required fields");
+              return false;
 
             }
            
-           $scope.subtotal=user.baseMealPrice*(1+(user.taxRate/100));
-           $scope.tip=user.baseMealPrice*(user.tipPercentage/100);
-           $scope.tipTotal = $scope.tip +  $scope.tipTotal;
-           $scope.mealCount++;
-           $scope.averageTip= $scope.tipTotal/$scope.mealCount;
-           $scope.total= $scope.subtotal +$scope.tip;
+           $scope.getMore($scope.user.tag);
       
         }
     }
 
-    $scope.reset();
-
-
-  });
+      
+      
+    });

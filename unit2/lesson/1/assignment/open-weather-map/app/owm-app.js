c85c64daa75b2e6fd91fb87c9b723e71d3d8b880
Juan <jualin289@gmail.com>
2014-07-22 22:54:59 -0400

    Adding routing assignment

---
+++ unit2/lesson/1/assignment/open-weather-map/app/owm-app.js
@@ -0,0 +1,36 @@
+angular.module('OWMApp', ['ngRoute'])
+ .value('owmCities', 
+        ['New York', 'Dallas', 'Chicago'])
+    .config(function($routeProvider){
+        $routeProvider.when('/', {
+            templateUrl : './home.html',
+            controller : 'HomeCtrl'
+        })
+        .when('/cities/:city', {
+            templateUrl : './city.html',
+            controller : 'CityCtrl'
+        });
+]		.when('/cities', {
+    templateUrl : './city.html',
+    controller : 'CityCtrl',
+    resolve : {
+        city: function(owmCities, $route, $location) {
+            var city = $route.current.params.city;
+            if(owmCities.indexOf(city) == -1 ) {
+                $location.path('/error');
+                return;
+            }
+            return city;
+        }
+    }
+});
+.when('/error', {
+    template : '<p>Error Page Not Found</p>'
+});
+    })
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+    .controller('CityCtrl', function($scope, city) {
+    $scope.city = city;
+});

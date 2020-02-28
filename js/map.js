e6ab162c779aae35c621e0cc1f02b13712ab284a
Juan Calderin <l0gicgates@users.noreply.github.com>
2020-02-28 13:19:15 -0500

    refactoring

--- js/map.js
+++ js/map.js
@@ -1,20 +1,17 @@
 (function initMap() {
-    
-    mapboxgl.accessToken = 'pk.eyJ1IjoianVhbGluMjg5IiwiYSI6ImNrNzNuYnV3ZTBkMmkzbHFxcjNlNnhzZnMifQ.dOEEFE00vYryzLt-Go6W6A';
-    var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/light-v10', //hosted style id
-    center: [-89.38, 39], // starting position
-    zoom: 3 // starting zoom
-    });
-
-    map.scrollZoom.disable();
-    map.addControl(new mapboxgl.NavigationControl());
-
     $(document).ready(function() {
         $('#projectMap').DataTable();
-    } );
+        mapboxgl.accessToken = 'pk.eyJ1IjoianVhbGluMjg5IiwiYSI6ImNrNzNuYnV3ZTBkMmkzbHFxcjNlNnhzZnMifQ.dOEEFE00vYryzLt-Go6W6A';
+        var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/light-v10', //hosted style id
+        center: [-89.38, 39], // starting position
+        zoom: 3 // starting zoom
+        });
 
+        map.scrollZoom.disable();
+        map.addControl(new mapboxgl.NavigationControl());
+        } );
 })()
     
     
